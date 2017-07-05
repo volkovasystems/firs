875103466e8c5380c01766a34f547c664c36d694
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-05 17:27:14 +0800

    Removed protype as dependency.

--- firs.js
+++ firs.js
@@ -37,7 +37,7 @@
 			"eMail": "richeve.bebedor@gmail.com",
 			"contributors": [
 				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
-				"Vinse Vinalon"
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
 			],
 			"repository": "https://github.com/volkovasystems/firs.git",
 			"test": "firs-test.js",
@@ -58,7 +58,6 @@
 			"arid": "arid",
 			"doubt": "doubt",
 			"falzy": "falzy","
-			"protype": "protype",
 			"raze": "raze",
 			"truly": "truly"
 		}
@@ -68,7 +67,6 @@
 const arid = require( "arid" );
 const doubt = require( "doubt" );
 const falzy = require( "falzy" );
-const protype = require( "protype" );
 const raze = require( "raze" );
 const truly = require( "truly" );
 
