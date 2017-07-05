f14bd7b2bcf93ce6c095ff78bd33c01f137785db
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-05 17:28:31 +0800

    Updated server/client test.

--- firs-test.js
+++ firs-test.js
@@ -4,6 +4,21 @@ const firs = require( "./firs.js" );
 
 assert.equal( firs( [ 1, 2, 3 ] ), 1, "should return 1" );
 
-assert.equal( firs( "hello world" ), "hello world", "should return 'hello world'" );
+assert.equal( firs( [ false, true ] ), false, "should return false" );
+
+assert.equal( firs( [ "", "yeah", { } ] ), "yeah", "should return 'yeah'" );
+
+assert.equal( firs( [ null, undefined, 2 ] ), 2, "should return 2" );
+
+assert.equal( firs( [ null, Symbol.for( "hello" ), true ] ), Symbol.for( "hello" ), "should be equal" );
+
+let test = function test( ){ return "test" };
+class ClassA{
+	constructor( ){ }
+	method( ){ return "hello"; }
+}
+
+let testA = new ClassA( );
+assert.deepEqual( firs( [ null, undefined, testA, test ] ), testA, "should be equal" );
 
 console.log( "ok" );
